b8812061abd44c7c786dc80eb1297312f3b48b2c
Bawantha <bawanthapasqual@gmail.com>
2024-12-23 09:14:01 +0530

    Setup

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 
 // app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// app.use("/", routes);
+app.use("/", routes);
 
 // const db = require("./models");
 
